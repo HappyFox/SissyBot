a7a9d6a5a618963fbf2133db9e62ba7cfe522691
Keith Baston <keith.baston@gmail.com>
2019-05-28 21:39:47 -0700

    settings working

--- sissyBot/client.py
+++ sissyBot/client.py
@@ -4,17 +4,56 @@ import kivy.app
 import kivy.uix
 import kivy.uix.widget
 import kivy.uix.boxlayout
+import kivy.properties
 
 from . import float_joy
 
 
 class RootWidget(kivy.uix.boxlayout.BoxLayout):
-    pass
+
+    address_label = kivy.properties.ObjectProperty(None)
+
+    def on_config_update(self, section, key, value):
+        print(section)
+        print(key)
+        print(value)
 
 
 class ClientApp(kv.app.App):
+    use_kivy_settings = False
+
     def build(self):
-        return RootWidget()
+        self.root = RootWidget()
+        return self.root
+
+    def build_settings(self, settings):
+        # {"type": "title", "title": "Networking"},
+        json_data = """
+        [
+
+            {
+                "type": "string",
+                "title": "Address",
+                "desc": "Destination address",
+                "section": "robot",
+                "key": "address"
+            },
+            {
+                "type": "numeric",
+                "title": "Port",
+                "desc": "Destination port",
+                "section": "robot",
+                "key": "port"
+            }
+        ]
+        """
+        settings.add_json_panel("Networking", self.config, data=json_data)
+
+    def build_config(self, config):
+        config.setdefaults("robot", {"address": "sissybot.local", "port": "4443"})
+
+    def on_config_change(self, config, section, key, value):
+        print(key + " " + value)
 
 
 def client():
